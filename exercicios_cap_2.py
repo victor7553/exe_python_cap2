c17ce2f0c87d5ef5fb1f6e768f1969b36e9744c7
Victor Hugo <69818265+victor7553@users.noreply.github.com>
2023-09-11 21:25:58 -0300

    Add files via upload

---
+++ exercicios_cap_2.py
@@ -0,0 +1,58 @@
+#capítulo 2
+
+
+print('\n------------- exercicio 2.1 --------------------')
+# Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+# 10 + 20 × 30
+# 42 ÷ 30
+# (94 + 2) × 6 - 1
+
+calc1 = 10 + 20 * 30
+calc2 = 42/30
+calc3 = (94 + 2) * 6 - 1
+
+print(calc1)
+print(calc2)
+print(calc3)
+
+
+print('\n------------- exercicio 2.2 --------------------')
+# Digite a seguinte expressão no interpretador:
+# 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+# Tente resolver o mesmo cálculo, usando apenas lápis e papel. Observe como a prioridade das operações é importante.
+
+calc = 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+print(calc)
+
+
+print('\n------------- exercicio 2.3 --------------------')
+# Faça um programa que exiba seu nome na tela.
+
+print('Victor Hugo Rodrigues Trindade')
+
+
+print('\n------------- exercicio 2.4 --------------------')
+# Escreva um programa que exiba o resultado de 2a × 3b, em que a vale 3 e b vale 5.
+
+a = 3
+b = 5
+print(2 * a * 3 * b)
+
+
+print('\n------------- exercicio 2.5 --------------------')
+# Escreva um programa que calcule a soma de três variáveis e imprima o resultado na tela.
+
+a = 2
+b = 3
+c = 5
+soma = a + b + c
+print(soma)
+
+
+print('\n------------- exercicio 2.6 --------------------')
+# Modifique o Programa 2.2, de forma que ele calcule um aumento de 15% para um salário de R$ 750.
+
+salario = 750
+aumento = 15
+print(salario + (salario * aumento) / 100)
+
